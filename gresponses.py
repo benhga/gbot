b93f8fcb827d6948ed43fd2661b76429d2c11290
Korstiaan <korstiaanw@genesis-analytics.com>
2020-05-26 10:40:01 +0000

    Added responses dictionary

---
+++ gresponses.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+Dictionary = {
+
+    # covid related information
+    "covid": "I can help you navigate through the *COVID* uncertainties \
+in this troublesome time. The options include:\
+\n\n\
+1. *IsAfricaFlatteningTheCurve* \U0001F4C8\n\
+2. *HealthcareRiskCalculator* \U000026D1\n\
+3. *CovidNews&Insights* \U0001F4E1\
+\n\n\
+Just press the number or type the hashtag that you want to know more about! \
+Alternatively, visit https://www.genesis-analytics.com/covid19 for all of our \
+COVID-19 related information.\n\n\
+You can return to this menu at any time by typing *COVID*.\
+\n\n\
+If you want more information on the pandemic in South Africa, you can WhatsApp \
+‘Hi’ to 0600 123 456 or visit www.sacoronavirus.co.za.\
+\n\n",
+
+    # initial introduction
+    "hello": "*Hello*! \U0001F44B\U0001F916\n\n\
+I am the G:Bot and I’m here to let you know of some of the ground-breaking work \
+done by Genesis Analytics, Africa’s largest economics-based consulting firm \
+\U0001F30D\
+\n\n\
+For more information in specific areas, you can type\
+\n\n\
+\U00002139 *About*\n\
+\U0001F9A0 *COVID*\n\
+\U0001F4E2 *News*\
+\n\n\
+You can also see the value we have unlocked with\
+\n\n\
+\U0001F5DD *Value*\
+\n\n\
+If none of these are what you're looking for, you can reach out to one of my \
+human associates by typing *Contact* \
+\n\n\
+I hope you find what you’re looking for and gain some valuable information.\
+\n\n",
+
+# gives information about Genesis
+"about": "Great! Let me tell you more about us. \
+\n\n\
+At Genesis, our purpose is to unlock value in Africa. We use our analytical \
+capabilities to improve decision-making and to reveal substantial opportunities for our \
+clients and society. We are the largest economics-based consulting firm in Africa. \
+We work in many different sectors and offer variety of services.\
+\n\n\
+If you want to learn more about what my human friends get up to, \
+you can visit https://www.genesis-analytics.com/about-us#who-we-are\
+\n\n",
+
+    # value unlocked
+    "value1": "Our projects have unlocked value across the board \U0001F513\
+\n\n\
+Our recent articles include: *",
+
+    'value2': "*\n\nIf you want to read more about projects like these, \
+visit https://www.genesis-analytics.com/value-unlocked-intro. \
+\n\n",
+
+
+    # news
+    "news": "Here are some of Genesis Analytics' recent headlines: \n\n",
+
+    # is africa flattening the curve
+    'isafricaflatteningthecurve': '*Is Africa Flattening the Curve* provides \
+an interactive dashboard reporting \
+on the spread of the virus, its fatality rate, and the response in testing \
+across all African countries that have over 100 cases \U0001F637\
+\n\n\
+Compare the rate of spread of the virus across multiple countries, include \
+your own selection of international benchmarks, or dive deeper into the spread\
+ at a provincial level in South Africa.\
+\n\n\
+The service can be found at https://www.genesis-analytics.com/covid19.\
+\n\n',
+
+    # healthcare risk calculator
+    'healthcareriskcalculator': 'The *Healthcare Risk Calculator* provides a \
+quick tool for estimating the virus\' strain on the healthcare system by forecasting \
+the amount of ICU beds that will be required over the next two weeks \
+\U0001F3E5\
+\n\n\
+Input your own data to see how your district, region or country\'s healthcare \
+risk profile will likely evolve over the next two weeks. This is a useful tool\
+ to support decisions about healthcare resources and lockdown levels.\
+\n\n\
+The *Health Care Risk Calculator* can be found at www.genesis-analytics.com/covid19\
+\n\n',
+
+    # covid news
+    "covidnews":"Here are some of our most recent COVID related headlines:\n\n",
+
+    # contact
+    'contact': 'Would you like to contact our \
+\n\n\
+\U0001F4BC *BDU* (Business Development Unit) \n\
+\U0001F454 *Careers* team\n\
+\U0001F3E2 *Offices* \
+\n\n\
+or would you like to make a *CorporateEnquiry*?\
+\n\n',
+
+    #BDU
+    'bdu': 'The *Business Development Unit* is responsible for the \
+development of new proposals; the preparation and resourcing for upcoming \
+opportunities; and engagement with clients and partners on potential collaboration. \
+\U0001F91D\
+\n\n\
+To get in touch with our *BDU*, you can email bdu@genesis-analytics.com or \
+visit https://www.genesis-analytics.com/contact-us/#business-development-unit\
+\n\n',
+
+    # Careers
+    'careers': 'For information about *careers* at Genesis, please contact our \
+recruitment specialists Cobie Hönicke and Zandile Shabangu \
+by emailing recruitment@genesis-analytics.com. \U0001F4C1\
+\n\n\
+We also offer *bursaries* for South African economics students from previously \
+disadvantaged backgrounds to pursue their studies to Master’s \
+level, as well as internships.\
+\n\n\
+You can find out more at https://www.genesis-analytics.com/contact-us/#careers',
+
+    # Offices
+    'offices': "We have offices around the globe. To find out how to contact \
+them, please visit https://www.genesis-analytics.com/contact-us/#offices",
+
+    # Corporate Enquiry
+    'corporate': 'For *corporate enquiries*, please contact our Chief \
+Operating Officer, Mike Olver at mikeo@genesis-analytics.com\
+\n\n\
+For media enquiries, please contact the head of marketing, Didi Moyle at \
+didim@genesis-analytics.com\
+\n\n\
+For more contact information, you can visit https://www.genesis-analytics.com/contact-us'
+}
