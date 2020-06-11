cf0b1e0b6d3ec66fdbf490727e90951c3ad23987
benhga <66001429+benhga@users.noreply.github.com>
2020-06-11 09:06:33 +0200

    Update app.py
    put back request validator for testing

--- app.py
+++ app.py
@@ -41,28 +41,28 @@ app = Flask(__name__)
 #     user_input = db.Column(db.String(4096))
 #     date = db.Column(db.String(4096))
 
-# # validates Twilio requests
-# def validate_twilio_request(f):
-#     """Validates that incoming requests genuinely originated from Twilio"""
-#     @wraps(f)
-#     def decorated_function(*args, **kwargs):
-#         # Create an instance of the RequestValidator class
-#         validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
+# validates Twilio requests
+def validate_twilio_request(f):
+    """Validates that incoming requests genuinely originated from Twilio"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Create an instance of the RequestValidator class
+        validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
 
-#         # Validate the request using its URL, POST data,
-#         # and X-TWILIO-SIGNATURE header
-#         request_valid = validator.validate(
-#             request.url,
-#             request.form,
-#             request.headers.get('X-TWILIO-SIGNATURE', ''))
+        # Validate the request using its URL, POST data,
+        # and X-TWILIO-SIGNATURE header
+        request_valid = validator.validate(
+            request.url,
+            request.form,
+            request.headers.get('X-TWILIO-SIGNATURE', ''))
 
-#         # Continue processing the request if it's valid, return a 403 error if
-#         # it's not
-#         if request_valid:
-#             return f(*args, **kwargs)
-#         else:
-#             return abort(403)
-#     return decorated_function
+        # Continue processing the request if it's valid, return a 403 error if
+        # it's not
+        if request_valid:
+            return f(*args, **kwargs)
+        else:
+            return abort(403)
+    return decorated_function
 
 # initialises some variables
 out_dict = {}
