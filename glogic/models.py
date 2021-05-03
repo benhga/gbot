d8759251c06c2469f79853d99d59e61bb2df144d
Ben <benh@genesis-analytics.com>
2021-05-03 09:34:44 +0100

    initial build, should work needs testing

--- glogic/models.py
+++ glogic/models.py
@@ -3,11 +3,11 @@ from datetime import datetime
 from . import db
 
 
-class User(db.Model):
-    __tablename__ = "Data_for_G:Bot"
+class Responses(db.Model):
+    __tablename__ = "responses"
 
     id = db.Column(db.Integer, primary_key=True)
-    number = db.Column(db.String(20), nullable=False, unique=True)
+    number = db.Column(db.String(20), nullable=False)
     response = db.Column(db.String, nullable=False)
     date_added = db.Column(db.DateTime, default=datetime.now)
 
