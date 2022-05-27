2c8036a64f3ad87d7857e8230257b4ad9a0050de
benhga <benh@genesis-analytics.com>
2022-05-27 16:23:28 +0200

    questions added

--- glogic/gresponses.py
+++ glogic/gresponses.py
@@ -11,14 +11,79 @@ Would you like to start? (y/n)\
 ",
 
     # initial introduction
-    "question1": "This is *question 1*. Please reply with the number of your selected option only. \n\n\
-1. A\n\
-2. B\n\
-3. C\n\
+    "question1": "What is your gender?\n\n\
+1. Male\n\
+2. Female\n\
+3. Other\n\
+4. Prefer not to say\n\
 ",
-    "question2" : "This is *question 2*",
+    "question2" : "Please confirm your race.\n\n\
+1. African\n\
+2. Coloured\n\
+3. Indian\n\
+4. White\n\
+5. Other\n\
+6. Prefer not to say\n\
+",
 
-    "question3":"This is *question 3*",
+    "question3":"Are you a South African citizen?\n\n\
+1. Yes\n\
+2. No\n\
+3. Prefer not to say\n\
+",
+    "question4": "How old are you?\n\n\
+1. 18-35 years old\n\
+2. 36-50 years old\n\
+3. 51-64 years old\n\
+4. Above 65 years old\n\
+",
+    "question5" : "Which province do you live in?\n\n\
+1. Eastern Cape\n\
+2. Free State\n\
+3. Gauteng\n\
+4. Kwazulu Natal\n\
+5. Limpopo\n\
+6. Mpumalanga\n\
+7. Northern Cape\n\
+8. North West\n\
+9. Western Cape\n\
+",
+    "question6": "What is your employment status?\n\n\
+1. Employed\n\
+2. Employed with a side hustle\n\
+3. Self-employed\n\
+4. Unemployed\n\
+",
+    "question7": "What sector are you employed in?\n\n\
+1. Public sector\n\
+2. Private Sector\n\
+3. Community based organisation\n\
+4. Unemployed\n\
+",
 
+    "question8" : "How much do you earn per WHAT?\n\n\
+1. No income\n\
+2. Less than R3 000\n\
+3. R3 001 - R5 000\n\
+4. R5 001 - R8 000\n\
+5. R8 001 - R12 000\n\
+6. R12 001 - R15 000\n\
+7. R15 001 - R20 000 \n\
+8. Over R20 000\n\
+9. Prefer not to say\n\
+",
+
+    "question9": "What sector are you employed in?\n\n\
+1. 0-Grade9\n\
+2. Grade9-less Matric\n\
+3. Completed Matric\n\
+4. Post Matric Qualification\n\
+",
+
+    "question10":"Do you have a disability?\n\n\
+1. Yes\n\
+2. No\n\
+3. Prefer not to say\n\
+",
     "thanks": "\U00002705 Thank you for completing the survey. "
 }
