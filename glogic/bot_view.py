216a7260f08440f08b528260315887707f621252
benhga <benh@genesis-analytics.com>
2022-05-31 14:56:10 +0200

    needs fixing, session not persisting

--- glogic/bot_view.py
+++ glogic/bot_view.py
@@ -1,4 +1,3 @@
-
 from . import app, db
 from .gresponses import Dictionary, survey_questions
 from .models import Responses
@@ -9,52 +8,58 @@ from datetime import datetime
 
 @app.route('/message', methods=['GET', 'POST'])
 def bot():
-
-    # del session['view']
-    # del session['q1']
-    # del session['q2']
+    # session.pop('view')
+    # session.pop('q1')
+    #
+    # session.pop('q2')
+    # session.modified = True
     # del session['q3']
-    
-    
-    
+
     num = request.form.get('From')
     num = num.replace('whatsapp:', '')
     incoming_msg = request.form.get('Body').lower()
 
+    print("INCOMING MSG: " + incoming_msg)
+
     resp = MessagingResponse()
     # msg = resp.message()
-    
+
     # if user_error(num):
     #     mydate = datetime.now()
     #     mon = mydate.strftime("%B")
     #     resp.message(f"Our records indicate that you have already completed the survey for {mon}. We look forward to hearing from you next month.")
     #     return str(resp)
-    
+
     if "view" in session:
+        print("Redirect to: " + session['view'])
         resp.redirect(url_for(session["view"]))
-    else:        
-    
+    else:
+
         if ('hi' in incoming_msg) or ('hello' in incoming_msg) or ('menu' in incoming_msg):
             # session['view'] = 'survey'
             out = Dictionary['welcome']
-            
+
         elif ('are you still working' in incoming_msg):
             out = "Yes, all is well"
-            
+
         elif 'y' in incoming_msg:
             out = survey_questions['question1']
             session['view'] = 'survey'
-            session['started'] = True
-            
+            session.modified = True
 
         else:
-            out = f"I'm sorry, I'm still young and don't understand your request. \
-    Please use the words in bold to talk to me."
+            out = f"I'm sorry, but there's been a problem. \
+Please say \"Hi\" to try again."
 
         resp.message(out)
         # try resp.message or other format where there's no msg.body
-        
+    if session:
+        print('*' * 20)
+        for i in session.keys():
+            print(i + ": " + str(session.get(i)))
+
     return str(resp)
+
 
 # checks to see if user is in DB and, if not, adds them
 def user_error(num):
--- glogic/survey_view.py
+++ glogic/survey_view.py
@@ -7,11 +7,14 @@ from .gresponses import Dictionary, survey_questions
 from .models import Responses
 from .send_airtime import send_airtime_after_survey
 from datetime import date, datetime
+
+from .survey_runner import do_survey
 from .utils import return_to_menu
 
 
 @app.route('/survey', methods=['GET', 'POST'])
 def survey():
+    session['view'] = 'survey'
     """
     Redirect for the careers view. Holds careers logic.
     :return str: response
@@ -20,59 +23,26 @@ def survey():
     num = num.replace('whatsapp:', '')
     incoming_msg = request.form.get('Body').lower()
 
-    for i in session.keys():
-        print(i)
     resp = MessagingResponse()
     msg = resp.message()
-    
-    if 'q3' in session:
-        db.save(Responses(num, session['q1'], session['q2'], session['q3']))
-        airtime = send_airtime_after_survey(num)
-        if airtime > 0:
-            out = "Thank you for your cooperation. We look forward to hearing from you again next month."
-        else:
-            out = "Unfortunately there has been an error getting you your airtime. Please contact XXXXXXXXXX to follow up."
-    else:
-        out = do_survey(num, incoming_msg)
-    
-    msg.body(out)
-    return str(resp)
 
-# fucntion for keeping track of survey space, quite hardcoded 
-def do_survey(num, incoming_msg):
-        
-    if session['started']:
-        if format_ans(incoming_msg)[1]:
-            if 'q1' not in session:
-                session['q1'], _ = format_ans(incoming_msg)
-                out = survey_questions['question2']
-            elif 'q2' not in session:
-                session['q2'], _ = format_ans(incoming_msg)
-                out = survey_questions['question3']
-            else: 
-                session['q3'], _ = format_ans(incoming_msg)
-                db.save(Responses(number=num, question_1=session['q1'], question_2=session['q2'], question_3=session['q3']))
-                del session['view']
-                del session['q1']
-                del session['q2']
-                del session['q3']
-                out = "Thank you for your cooperation DO AIRTIME"
+    out, done = do_survey(incoming_msg)
+
+    msg.body(out)
+
+    if done:
+        session.pop('view')
+        session.pop('q1')
+        session.pop('q2')
+        session.pop('q3')
+
+        if len(session) > 0:
+            print("DELETION UNSUCCESSFUL")
         else:
-            out, _ = format_ans(incoming_msg)
-    else:
-        out = Dictionary['question1']
-        session['started'] = True
-    
-    return out
-        
-def format_ans(incoming_msg):
-    if '1' in incoming_msg:
-        return 1, True
-    elif '2' in incoming_msg:
-        return 2, True
-    elif '3' in incoming_msg:
-        return 3, True
-    elif '4' in incoming_msg:
-        return 4, True
-    else:
-        return 'Not sure what you\'re saying, please type number', False
+            print("NO SESSION VARS")
+
+    print('*' * 20)
+    for i in session.keys():
+        print(i + ": " + str(session.get(i)))
+
+    return str(resp)
