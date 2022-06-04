2bb1fbe0cf7fa934182f01726d1c5b7a54d53074
benhga <benh@genesis-analytics.com>
2022-06-04 12:48:19 +0200

    airtime sending

--- glogic/send_airtime.py
+++ glogic/send_airtime.py
@@ -1,24 +1,39 @@
 import os
 
 import africastalking
+import pandas as pd
+
+def multiple_send():
+    nums = pd.read_csv("Airtime 29+30.xlsx - Sheet1.csv")
+    print(len(nums))
+    for i in nums['Phone Numbers']:
+        num = "+" + str(i)
+        send_airtime_after_survey(num)
+
 
 
 def send_airtime_after_survey(num):
-    username = os.environ.get("AT_USERNAME")
-    api_key = os.environ.get("AT_API_KEY")
-
+    # username = os.environ.get("AT_USERNAME")
+    # api_key = os.environ.get("AT_API_KEY")
+    username = "ASIS038"
+    api_key = "27977c5e42f686f7e2097b296f42ca041a2bd45bed94489ee3ef1c0d11779ae3"
     africastalking.initialize(username, api_key)
 
     airtime = africastalking.Airtime
 
     phone_number = num
     currency_code = "ZAR"  # Change this to your country's code
-    amount = 5
+    amount = 15
 
     try:
         response = airtime.send(phone_number=phone_number, amount=amount, currency_code=currency_code)
+        print('*'*20)
+        print(f"Airtime sent to {num}")
         print(response)
         return 1
     except Exception as e:
         print(f"Encountered an error while sending airtime. More error details below\n {e}")
         return -1
+
+if __name__ == "__main__":
+    multiple_send()
