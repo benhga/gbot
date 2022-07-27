6959a56fbd411c9125a1751db9205ffa409cb73a
benhga <benh@genesis-analytics.com>
2022-07-27 18:01:01 +0200

    test nums in and ready

--- bulk_send.py
+++ bulk_send.py
@@ -1,5 +1,6 @@
 # works with both python 2 and 3
 from __future__ import print_function
+import pandas as pd
 
 import africastalking
 
@@ -19,11 +20,24 @@ class SMS:
     def send(self):
             # Set the numbers you want to send to in international format
             # recipients = ["+27833888281", "+27780730005", "+27618129719"]
-            recipients = ["+27833888281"]
+
             # 827245713
+            nums = pd.read_csv("test_nums.csv")
+
+            num_l = nums.numbs.values.tolist()
+            cleaned = []
+            for i in num_l:
+                i = i[1:]
+                i = i.replace(" ", "")
+                i = i.replace(" ", "")
+                i = "+27" + i
+                cleaned.append(i)
+
+            recipients = cleaned
 
             # Set your message
             message = "Genesis invites you to the WageWise WhatsApp study. Earn R75 airtime by answering 15 questions. Click the link and send “Hi” https://wa.me/27600185052?text=Hi";
+
 
             # Set your shortCode or senderId
             sender = "GENESIS"
