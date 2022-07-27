1c4ab6c1283def6a61ba67cf45f03604e0ecd828
benhga <benh@genesis-analytics.com>
2022-07-27 15:27:13 +0200

    stop updated

--- glogic/bot_view.py
+++ glogic/bot_view.py
@@ -28,8 +28,8 @@ def bot():
     if "stop" in incoming_msg:
         resp.message("We will be sad to see you go. BETTER MESSAGE HERE")
 
-        user = User.query.filter(User.number==num).first()
-        User.delete(user)
+        user = User.query.filter(User.number==num).delete()
+        # User.delete(user)
         db.session.commit()
 
         return resp.message()
