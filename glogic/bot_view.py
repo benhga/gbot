c927732d16e0ce2952c75168471645dbb8126e42
benhga <benh@genesis-analytics.com>
2023-04-19 12:11:11 +0100

    bot_view.py revised for handover

--- glogic/bot_view.py
+++ glogic/bot_view.py
@@ -10,6 +10,7 @@ from bulk_sending.template_send import check_user_in_airtime_list
 @app.route('/message', methods=['GET', 'POST'])
 def bot():
 
+    # for testing
     # del session['view']
     # del session['otp']
     # del session['question_id']
@@ -23,8 +24,6 @@ def bot():
     print("INCOMING MSG: " + incoming_msg)
 
     resp = MessagingResponse()
-    # msg = resp.message()
-
 
     if 'stop' in incoming_msg:
         resp.message("We will be sad to see you go.")
@@ -33,16 +32,11 @@ def bot():
         db.session.commit()
         return str(resp)
 
-    # if registered(num) == 0:
-    #     resp.message("Your number is not in our records. Please contact digital@genesis-analytics.com if you believe this to be an error")
-    #     return str(resp)
-
     if "view" in session:
         print("Redirect to: " + session['view'])
         resp.redirect(url_for(session["view"]))
     else:
-        # resp.message("The registration period has ended. If you have registered, you will be notified when a new monthly survey is available.")
-        # if num[1:] in numbers_list:
+        # for airtime corrections
         if check_user_in_airtime_list(num):
             out ="Hi! You have opted into the WageWise survey brought to you by Genesis Analytics. We understand that you have been having issues receiving your R17 airtime for completing the survey."
             out += " Please follow this simple process for us to verify your phone number and get your airtime to you.\n\n"
@@ -50,52 +44,28 @@ def bot():
 
             session['view'] = 'correct_number'
 
+        # main entry point for monthly surveys
         elif ('hi' in incoming_msg) or ('hello' in incoming_msg) or ('menu' in incoming_msg) or ('ok' in incoming_msg) or ("yes" in incoming_msg):
-            # resp.message(Dictionary['welcome1'])
-#             if num == "+27822205729":
-#             out = "The registration period has ended. If you have registered, you will be notified when a new monthly survey is available."
 
             reg = registered(num)
             if reg == 1:
+                # survey starts
                 session['view'] = 'survey'
                 session['count'] = 0
                 out = "Thank you for participating in the WageWise 3 year survey. " + Dictionary['welcome3']
 
-
-
-                # out = Dictionary['welcome2'] + "\n\n" + Dictionary['welcome3']
-                # session['view'] = 'baseline'
-            # elif reg == -1:
-            #     out = "Your phone number is not in our database. If you have participated in the WageWise program and completed the registration, please contact digital@genesis-analytics.com."
             else:
                 out = "Your phone number is not in our database. If you have participated in the WageWise program and completed the registration, please contact digital@genesis-analytics.com."
-
-                # resp.message("You have completed your registration.")
-                # out = "You will be notified when a new monthly survey is available."
-
 
 
         elif ('are you still working' in incoming_msg):
             out = "Yes, all is well"
 
-
-        # if True:
-        #     out = "Hi, the bot is currently inactive. If you were part of the WageWise program, you will be notified when new surveys are ready."
-
-        # elif "thank" in incoming_msg:
-        #     out = "You're welcome :)"
         
         else:
             out = f"I'm sorry, but there's been a problem. \
 Please say \"Hi\" to try again."
-
-        # out = "We are unfortunately experiencing problems with our airtime provider. To ensure that you receive your reward, we ask that you please wait for our invitation to complete the November survey."
         resp.message(out)
-        # try resp.message or other format where there's no msg.body
-    # if session:
-    #     print('*' * 20)
-    #     for i in session.keys():
-    #         print(i + ": " + str(session.get(i)))
 
     return str(resp)
 
@@ -109,28 +79,14 @@ def user_error(num):
 
 def registered(num):
     """
-    Returns -1 if user not in demos, 0 if user is not registered, 1 if registered
+    Returns 0 if user is not registered, 1 if registered
     """
     user = User.query.filter(User.number == num).first()
-
-    # allowed, _ = get_data(num)
-    # print("Allowed: ", allowed)
-    # if allowed:
-    # if True:
     if user is not None:
         if user.registered == 1:
             return 1
         else:
             return 0
     else:
-        # allowed, num2 = get_data(num)
-        # # print(allowed, num2)
-        # if allowed:
-        #     db.save(User(number=num, number_2=num2))
         return 0
 
-
-    return -1
-
-def invalid_user(num):
-    demos = pd.read_csv("../demographics/ww_test_csv.csv")
